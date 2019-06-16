8c2dfc611e5fa63c503dd604a2e624e5a2175890
Emmanuel Agubata <emmanuel@Ogos-MacBook-Pro.local>
2019-06-15 17:00:40 -0700

    this is a completed astro-weight calculators

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- Astro-Weight Calculator
+This project is able to convert your curent weight on Earth and convert it to a new weight on any given planet in our solar system. 
+First you figure out your weight
+Next you enter in your cuurent weight in the user weight section.
+Then you choose the planet that you want to convert your weight to
+Lastly click the calculate button then sit back and have a good laugh. -->
--- main.js
+++ main.js
@@ -1,1 +1,65 @@
-// Write your JavaScript code here!
+// / Write your JavaScript code here!
+var planets = [
+  ['Pluto', 0.06],
+  ['Neptune', 1.148],
+  ['Uranus', 0.917],
+  ['Saturn', 1.139],
+  ['Jupiter', 2.640],
+  ['Mars', 0.3895],
+  ['Moon', 0.1655],
+  ['Earth', 1],
+  ['Venus', 0.9032],
+  ['Mercury', 0.377],
+  ['Sun', 27.9]
+];
+
+
+
+planets.forEach(function(element){
+var dropdown = document.getElementById('planets');
+var option = document.createElement('option');
+var txt = document.createTextNode(element[0]);
+    option.appendChild(txt);
+    dropdown.insertBefore(option,dropdown.lastChild);
+    //console.log(option);
+});
+
+
+
+function calculateWeight(userWeight, planetName){
+
+  var userWeight = document.getElementById('user-weight').value;
+  var planetName = document.getElementById('planets').selectedIndex -1;
+  var gravity = planets[planetName][1];
+  result = userWeight * gravity;
+
+
+console.log(userWeight);
+console.log(planetName);
+console.log(gravity);
+console.log(result);
+return result; 
+};
+
+
+// 3. Create a variable called userWeight and assign
+// the value of the user’s weight.
+// 4. Create a variable called planetName and assign
+// the name of the selected planet from the drop down.
+// 5. Create a variable called result and assign the
+// value of the new calculated weight.
+// 6. Write code to display the message shown in the
+// screenshot.
+
+
+
+function handleClickEvent() {
+    var userWeight = document.getElementById('user-weight').value;
+    var planetName = document.getElementById('planets').value;
+    result = calculateWeight(userWeight, planetName);
+    //console.log(result);
+    //console.log(userWeight);
+    //console.log(planetName);
+    //document.getElementById("output").innerHTML = "If you were on " +planetName+ ", you would weigh "  +result+ " lbs!";
+    document.getElementById("output").innerHTML = "If you were on " + planetName+ ", you would weigh " + result+ "lbs!";
+}
